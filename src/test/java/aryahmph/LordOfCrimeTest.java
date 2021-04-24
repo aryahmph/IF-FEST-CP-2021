63b136e7d32767fcbe57a960096cfb9b53c0f1c6
Arya Yunanta <aryayun90@gmail.com>
2021-04-24 11:36:50 +0700

    Add more unit test for Lord of Crime question

--- src/test/java/aryahmph/LordOfCrimeTest.java
+++ src/test/java/aryahmph/LordOfCrimeTest.java
@@ -31,11 +31,22 @@ public class LordOfCrimeTest {
     @Test
     @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
     void testLordOfCrime3() {
-        String[] names = {"Charles","Ciel", "Ciel", "Albert", "Albert", "Albert", "Albert", "Albert",
-                "Albert","Albert", "Albert", "Albert"};
+        String[] names = {"Charles", "Ciel", "Ciel", "Albert", "Albert", "Albert", "Albert", "Albert",
+                "Albert", "Albert", "Albert", "Albert"};
         String[] res = LordOfCrimeSolution.lordOfCrime(names);
 
         assertArrayEquals(new String[]{"Albert", "9"}, res);
+
+    }
+
+    @Test
+    @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
+    void testLordOfCrime4() {
+        String[] names = {"William", "William", "Charles", "Ciel", "William", "William", "Ciel", "William", "Ciel", "Ciel", "William", "William", "Ciel", "William", "Ciel", "William", "Ciel", "William", "William", "Ciel", "Ciel", "Charles", "Ciel", "William", "Ciel", "William", "Ciel", "William", "William", "Charles", "William", "Ciel", "William", "William", "William", "William", "Charles", "Ciel", "Ciel", "Ciel", "William", "William", "Ciel", "William", "Charles", "William", "William", "Ciel", "William", "Charles", "Moriarty", "William", "Moriarty", "William", "William", "William", "William", "William", "William", "Ciel", "William", "William", "William", "Moriarty", "Charles", "William", "Ciel", "William", "William", "William"};
+        // William 40, Ciel = 20, Moriarty = 3, Charles = 7
+        String[] res = LordOfCrimeSolution.lordOfCrime(names);
+
+        assertArrayEquals(new String[]{"William", "40"}, res);
 
     }
 }
