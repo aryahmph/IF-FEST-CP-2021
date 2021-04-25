6ad020604c0d3ca5e640bba7d2e7321b56af00f6
Arya Yunanta <aryayun90@gmail.com>
2021-04-25 21:18:54 +0700

    Add another Lord of Crime solution

--- src/main/java/aryahmph/LordOfCrimeSolution.java
+++ src/main/java/aryahmph/LordOfCrimeSolution.java
@@ -1,6 +1,8 @@
 package aryahmph;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LordOfCrimeSolution {
@@ -30,5 +32,33 @@ public class LordOfCrimeSolution {
         }
 
         return new String[]{maxKey, Integer.toString(map.get(maxKey))};
+    }
+
+    public static List<String> lordOfCrime(List<String> names) {
+        int namesLen = names.size();
+        Map<String, Integer> map = new HashMap<>(namesLen);
+        for(String name : names) {
+            if (map.containsKey(name)) {
+                map.put(name, map.get(name) + 1);
+            } else {
+                map.put(name, 1);
+            }
+        }
+        // Logic
+        String maxKey = names.get(0);
+        int maxVal = map.get(maxKey);
+
+        // Search largest value from key
+        for (String key : map.keySet()) {
+            if (map.get(key) > maxVal) {
+                maxKey = key;
+                maxVal = map.get(maxKey);
+            }
+        }
+
+        List<String> res = new ArrayList<>();
+        res.add(maxKey);
+        res.add(Integer.toString(map.get(maxKey)));
+        return res;
     }
 }
